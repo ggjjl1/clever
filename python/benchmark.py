2a00dc66f2b10b4da8cbf5cb10fc3cd277da30dd
gaojunliang <gaojunliang@soulapp.cn>
2021-09-13 15:20:19 +0800

    python性能测试

---
+++ python/benchmark.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import time
+
+if __name__ == '__main__':
+    # python执行1.5亿次循环耗时时间
+    counter = 0
+    start = int(time.time() * 1000)
+    for i in range(150000000):
+        counter = counter + 1
+
+    end = int(time.time() * 1000)
+    print("执行时间：" + str(end - start) + "毫秒")
